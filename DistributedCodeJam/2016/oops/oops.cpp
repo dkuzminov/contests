1810888620dc93d0fb5af8a1317a57e797a14f02
Dmitry Kuzminov <dmitry.kuzminov@gmail.com>
2017-05-07 21:45:23 -0400

    Distributed Code Jam 2016, Round 1   NB! Both problems (oops, rps) are valid for small datasets only

---
+++ DistributedCodeJam/2016/oops/oops.cpp
@@ -0,0 +1,73 @@
+#include <message.h>
+#include <stdio.h>
+#include <climits>
+#include "oops.h"
+
+#define MASTER_NODE 7
+#define DONE -1
+
+int main() {
+  long long N = GetN();
+  long long nodes = NumberOfNodes();
+  long long my_id = MyNodeId();
+  long long _min = LLONG_MAX;
+  long long _max = LLONG_MIN;
+  int found = 0;
+  for (long long i = 0; i < N; ++i) {
+      if (i % nodes == my_id) {
+          long long number = GetNumber(i);
+          if(found == 0) {
+              found = 1;
+              _min = number;
+              _max = number;
+          }
+          else {
+            if(_min > number) {
+                _min = number;
+            }
+            if(_max < number) {
+                _max = number;
+            }
+          }
+      }
+  }
+  if (found == 1) {
+    PutLL(MASTER_NODE, 1);
+    PutLL(MASTER_NODE, _min);
+    PutLL(MASTER_NODE, _max);
+  }
+  PutLL(MASTER_NODE, DONE);
+  Send(MASTER_NODE);
+
+  if (my_id == MASTER_NODE) {
+    long long global_min = LLONG_MAX;
+    long long global_max = LLONG_MIN;
+    int found = 0;
+    for (int node = 0; node < nodes; ++node) {
+      long long received1 = 0;
+      long long received2 = 0;
+      Receive(node);
+      received1 = GetLL(node);
+      if(received1 == 1) {
+            received1 = GetLL(node);
+            received2 = GetLL(node);
+            if(found == 0) {
+                found = 1; 
+                global_min = received1;
+                global_max = received2;
+            }
+            else {
+                if(global_min > received1) {
+                    global_min = received1;
+                }
+                if(global_max < received2) {
+                    global_max = received2;
+                }
+            }
+            GetLL(node);
+      }
+    }
+    printf("%lld\n", global_max - global_min);
+  }
+  return 0;
+}
---
+++ DistributedCodeJam/2016/rps/rps.cpp
@@ -0,0 +1,81 @@
+#include <message.h>
+#include <stdio.h>
+#include <deque>
+#include "rps.h"
+using namespace std;
+
+#define MASTER_NODE 7
+
+long long winner(long long i, long long j){
+    char c1 = GetFavoriteMove(i);
+    char c2 = GetFavoriteMove(j);
+    if(c1 == c2)
+        return i;
+    if(c1 == 'R' && c2 == 'S')
+        return i;
+    if(c1 == 'P' && c2 == 'R')
+        return i;
+    if(c1 == 'S' && c2 == 'P')
+        return i;
+    return j;
+}
+
+int main() {
+    long long N = GetN();
+    long long nodes = NumberOfNodes();
+    long long my_id = MyNodeId();
+    long long div = 8;
+    long long players = 1LL << N;
+
+    if(nodes >= 64) {
+        div = 64;
+    }
+    div = 1;
+
+    long long beg = (players / div) * my_id;
+    long long end = (players / div) * (my_id + 1);
+    if(end > players)
+        end = players;
+
+    if(beg >= players || beg == end) {
+        PutLL(MASTER_NODE, -1);
+    }
+    else {
+        PutLL(MASTER_NODE, 1);
+        deque<long long> d1(end - beg);
+        for (long long i = beg; i < end; ++i) {
+            d1[i - beg] = i;
+        }
+        while(d1.size() > 1) {
+            deque<long long> d2;
+            for (long long i = 0; i < d1.size(); i+=2) {
+                d2.push_back(winner(d1[i], d1[i+1]));
+            }
+            swap(d1, d2);
+        }
+        PutLL(MASTER_NODE, d1[0]);
+    }
+    Send(MASTER_NODE);
+
+    if (my_id == MASTER_NODE) {
+        deque<long long> d1;
+        for (int node = 0; node < nodes; ++node) {
+            Receive(node);
+            long long received = GetLL(node);
+            if(received == 1) {
+                received = GetLL(node);
+                d1.push_back(received);
+            }
+        }
+
+        while(d1.size() > 1) {
+            deque<long long> d2;
+            for (long long i = 0; i < d1.size(); i+=2) {
+                d2.push_back(winner(d1[i], d1[i+1]));
+            }
+            swap(d1, d2);
+        }
+        printf("%lld\n", d1[0]);
+    }
+    return 0;
+}
